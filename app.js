1af054d584d2490622aba2a50b5ec3014a6f5409
jeronimogarcia <jero_garcia23@hotmail.com>
2022-08-23 13:45:53 -0300

    contador

--- app.js
+++ app.js
@@ -1,25 +1,18 @@
-var app = new Vue ({
-    el: "#app",
-    data:{
-        message: 'Hello World',
-        num1: 5,
-        num2: 6
+var app = new Vue({
+  el: "#app",
+  data: {
+    welcomeMessage: "Vue JS - Trabajo Práctico Uno",
+    counter: 0,
+  },
+  methods: {
+    incrementar() {
+      this.counter++;
     },
-    methods:{
-        imprimir(){
-            console.log('Hello Coder')
-        },
-        calculo(){
-            let a = 3;
-            let b = 5;
-            console.log(a*b)
-        }
+    decrementar() {
+      if (this.counter > 0) {
+        this.counter--;
+      }
     },
-    computed:{
-        calculoMatematico(){
-            return (this.num1*this.num2)
-        }
-    }
-
-})
-
+  },
+  computed: {},
+});
