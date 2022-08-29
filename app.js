9be02cde9a86b3501ab267ddd9bd77694d99c54f
jeronimogarcia <jero_garcia23@hotmail.com>
2022-08-29 03:24:06 -0300

    clase dos

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
-var app = new Vue ({
-    el: "#app",
-    data:{
+var app = new Vue({
+  el: "#app",
+  data: {
+    message: "Hola coder!",
+    num: 0,
+    sum: 10,
+    bool: true,
+    style: "green",
+  },
+  methods: {
+    imprimir() {
+      console.log("321");
     },
-    methods:{ 
+    sumar() {
+      this.num = parseInt(this.num) + parseInt(this.sum);
+      this.num = +this.num + this.sum;
+      //this.num++
     },
-    computed:{
-    }
-})
-
+    restar() {
+      this.num = parseInt(this.num) - parseInt(this.sum);
+      //this.num--
+    },
+  },
+  computed: {
+    text() {
+      return "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+    },
+    text2() {
+      return `<span style="color:red;">coder</span>`;
+    },
+    calculoMatematico() {
+      return true ? this.num1 : this.num2;
+    },
+  },
+});
