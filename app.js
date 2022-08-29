709748ee7693816b5340f4eef9aab3da8df403d2
jeronimogarcia <jero_garcia23@hotmail.com>
2022-08-29 03:22:07 -0300

    main sin nada

--- app.js
+++ app.js
@@ -1,25 +1,10 @@
 var app = new Vue ({
     el: "#app",
     data:{
-        message: 'Hello World',
-        num1: 5,
-        num2: 6
     },
-    methods:{
-        imprimir(){
-            console.log('Hello Coder')
-        },
-        calculo(){
-            let a = 3;
-            let b = 5;
-            console.log(a*b)
-        }
+    methods:{ 
     },
     computed:{
-        calculoMatematico(){
-            return (this.num1*this.num2)
-        }
     }
-
 })
 
