1fc6c3e3d2a76e316b8238c9322e0eacc9cc34c8
jeronimogarcia <jero_garcia23@hotmail.com>
2022-09-21 06:31:59 -0300

    form

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,7 @@
 <template>
-  <div id="app">
-    <div class="container">
-      <CoderCards 
-      v-for="(meal, index) in codermeals" 
-      :index="index" 
-      :key="meal.id" 
-      :meal="meal"
-      @deleteMeal="deleteMeal"
-      />
-    </div>
-  </div>
 </template>
 
 <script>
-import CoderCards from './components/CoderCards.vue';
-
-export default {
-  name: 'App',
-  components: {
-    CoderCards,
-  },
-  data() {
-    return {
-      codermeals: [
-        {
-          id: 1,
-          titulo: "Spaghetti alla puttanesca",
-          costo: 575.0,
-        },
-        {
-          id: 2,
-          titulo: "Pizza Napoletana ai carciofi",
-          costo: 675.0,
-        },
-        {
-          id: 3,
-          titulo: "Porchetta umbra a cottura lunga",
-          costo: 845.0,
-        },
-        {
-          id: 4,
-          titulo: "Orecchiette alle cime di rapa",
-          costo: 845.0,
-        },
-      ],
-    }
-  },
-  methods: {
-    deleteMeal(i) {
-      this.codermeals.splice(i, 1)
-    },
-  }
-}
 </script>
 
 <style scope>
@@ -64,9 +14,5 @@ export default {
   position: relative;
   min-height: 91.5vh;
   padding-bottom: 80px;
-}
-
-.container {
-  display: flex;
 }
 </style>
