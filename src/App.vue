e3dabdb54aa8c5458f13dfb85c44ecae21b9eaca
jeronimogarcia <jero_garcia23@hotmail.com>
2022-09-22 10:49:38 -0300

    vueclivacio

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,9 @@
 <template>
   <div id="app">
-    <Header :title='title' :listaCarrito='listaCarrito'/>
-    <h2 class="titleHome">{{titleHome}}</h2>
-    <div class="cardContainer">
-      <CoderCards v-for="meal in codermeals" :key="meal.id" :meal="meal" :listaCarrito='listaCarrito' />
-    </div>
-    <div v-if="listaCarrito.length!=0">
-      <CarritoCards  :listaCarrito="listaCarrito" :carritoTitle="carritoTitle" />
-    </div>
-    <Footer :footerTitle='footerTitle' />
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue';
-import CoderCards from './components/CoderCards.vue';
-import CarritoCards from './components/CarritoCards.vue';
-import Footer from './components/Footer.vue';
-
-
-
-export default {
-  name: 'App',
-  components: {
-    Header,
-    CoderCards,
-    CarritoCards,
-    Footer,
-},
-  data() {
-    return {
-      title: 'CoderMeals - VUE/CLI ',
-      carritoTitle: 'Lista Carrito',
-      footerTitle: 'CoderHouse - CoderMeals',
-      titleHome: 'Nuestras Comidas',
-      listaCarrito: [],
-      codermeals: [
-        {
-          id: 1,
-          titulo: "Spaghetti alla puttanesca",
-          costo: 575.0,
-          portada:
-            "https://4.bp.blogspot.com/-D5Wvi_gX_Kg/WLatk_GVnKI/AAAAAAAAA5M/-y0gB26R0Dkb01QmdgfZqQALtd9NMV2DACLcB/s1600/P70301-072354.jpg",
-          counter: 1,
-          quantity: 5,
-        },
-        {
-          id: 2,
-          titulo: "Pizza Napoletana ai carciofi",
-          costo: 675.0,
-          portada:
-            "https://static.cookist.it/wp-content/uploads/sites/21/2017/12/istock-480277738.jpg",
-          counter: 1,
-          quantity: 6,
-        },
-        {
-          id: 3,
-          titulo: "Porchetta umbra a cottura lunga",
-          costo: 845.0,
-          portada:
-            "https://www.fontecesia.it/wp-content/uploads/2018/11/porchetta-umbra-1920x1280.jpg",
-          counter: 1,
-          quantity: 10,
-        },
-        {
-          id: 4,
-          titulo: "Orecchiette alle cime di rapa",
-          costo: 845.0,
-          portada:
-            "https://irepo.primecp.com/2016/03/259860/recipe-8673_ExtraLarge1000_ID-1461628.jpg",
-          counter: 1,
-          quantity: 4,
-        },
-      ],
-    }
-  }
-}
 </script>
 
 <style>
@@ -88,19 +16,5 @@ export default {
   position: relative;
   min-height: 91.5vh;
   padding-bottom: 80px;
-}
-
-.cardContainer {
-  display: flex;
-  flex-wrap: wrap;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-  
-}
-
-.titleHome{
-  padding-top: 75px;
-  font-size: 28px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import Vuelidate from 'vuelidate'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
-library.add(faCartShopping)
-Vue.component('font-awesome-icon', FontAwesomeIcon)
+import { BootstrapVue } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+library.add()
 
+Vue.use(VueRouter)
+Vue.use(Vuelidate)
+Vue.use(BootstrapVue)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.config.productionTip = false
+
 
 new Vue({
   render: h => h(App),
